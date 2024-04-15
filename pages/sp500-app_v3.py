3388f7589121cfe10f7f1f65a84e86d64b1f8d30
peteciank <106826936+peteciank@users.noreply.github.com>
2024-04-15 15:04:57 -0300

    d

--- pages/sp500-app_v3.py
+++ pages/sp500-app_v3.py
@@ -10,12 +10,11 @@ st.set_page_config("S&P 500 Webscrap - v1", "💹", layout="wide")
 menu()
 def main():
 
-    st.title('S&P 500 - List of Companies')
-
     st.markdown("""
-                This app retrieves the list of the **S&P 500** (from a pre-existing dataset in github).
-                * **Python libraries:** base64, pandas, streamlit, pandas_datareader, numpy
+                This app retrieves the list of the **S&P 500** from Wikipedia, using Webscraping.
+                * **Python libraries:** base64, pandas, streamlit, pandas_datareader, numpy, matplotlib, beautifulSoup
                 """)
+
 
 
     if st.button('Fetch S&P 500 Companies'):
